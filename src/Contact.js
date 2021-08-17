6e246f4eb3bcc0592c41c673c3329d6565da444e
dhybit5014 <dhyrlbitamor@gmail.com>
2021-08-16 21:22:26 -0400

    Added Contact page

--- src/Contact.js
+++ src/Contact.js
@@ -6,8 +6,9 @@ class Contact extends Component {
   render() {
     return (
       <div className="container">
-        <h2 className="header">Contacts currently unavailable.</h2>
+        <h2 className="header">Contact page is currently unavailable</h2>
         <img className="UnderMaintenance" src={image9} />
+        <h2 className="header">Sorry for the inconvenience</h2>
       </div>
     );
   }
